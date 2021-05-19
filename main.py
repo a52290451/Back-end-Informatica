0bbb14225fd02af7373985241f3a2540b564724c
milo1998 <camiloerc_18@hotmail.com>
2021-05-18 21:59:33 -0500

    Cambios Main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from flask_cors import CORS
 
 
 
+app = Flask(__name__)
+
+cors = CORS(app,resources={r"/filtroParametros/*": {"origins": "*"}})
+
+
 filtro = [
    {
        "Categoria": "Revista 1",
@@ -52,7 +57,84 @@ def getFiltro():
 
 
 
-app = Flask(__name__)
-if __name__ == '__main__':
+
+#------------------ Métodos de prueba --------------------
+
+users = [
+   {
+       "Username": "Admin",
+       "Age": "25"
+
+   },
+   {
+       "Username": "user 1",
+       "Age": "18"
+
+   } 
+]
+
+#ruta que captura petición, filtra por nombre de usuario y retorna la información de ese usuario
+@app.route('/users/<string:username>', methods =["GET"])
+def getUsersByUsername(username):
+    result = next((user for user in users if user["Username"] == username),None)
+    if result is not None:
+        return jsonify(result), 200
+    else:
+        return "User not found", 404
+
+#ruta que captura petición, filtra por nombre de usuario y retorna la información de ese usuario
+@app.route('/filtro/<string:categoria>/<string:cuartil>/<int:cantidad>', methods =["GET"])
+def getRevistasByCategoria(categoria):
+    result = next((comp for comp in filtro if comp["Categoria"] == categoria),None)
+    if result is not None:
+        return jsonify(result), 200
+    else:
+        return "User not found", 404
+def getRevistasByCuartil(cuartil):
+    result = next((comp for comp in filtro if comp["Cuartil"] == cuartil),None)
+    if result is not None:
+        return jsonify(result), 200
+    else:
+        return "User not found", 404
+
+#ruta que captura petición y devuelve una lista de usuarios
+@app.route('/users', methods =["GET"])
+def getAllUsers():
+    return jsonify(users), 200
+
+#Crear usuarios
+@app.route('/users', methods =["POST"])
+def addUser():
+    body = json.loads(request.data)
+    
+    userName = body["Username"]
+    age = body["Age"]
+    
+    newUser = {
+        "Username": userName,
+        "Age": age
+    }
+    
+    users.append(newUser)
+    return jsonify(newUser), 200
+
+#Borrar usuarios
+
+@app.route('/users/<string:username>', methods=["DELETE"])
+def deleteUser(username):
+    userFound = None
+    for index, user in enumerate(users):
+        if user["Username"] == username:
+            userFound = user
+            users.pop(index)
+    if userFound is not None:
+        return "User deleted", 200
+    else:
+        return "User not found", 404
+
+
+
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
-cors = CORS(app,resources={r"/filtroParametros/*": {"origins": "*"}})
